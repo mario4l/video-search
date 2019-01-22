8e04c7d89429665e10499796e1a7b39857d7c0c5
Mariano Vargas <mario.vargas29@gmail.com>
2019-01-21 16:54:04 -0800

    inital commit with created components

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = "AIzaSyAn-mXeSkavjZQvtBVHJY74WlMWpvSCGD0";
+
+export default axios.create({
+  // base url and default parameters
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
