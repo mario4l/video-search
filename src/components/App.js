083ceaf0fec37ba800fb11451a12043d78c510fc
Mariano Vargas <mario.vargas29@gmail.com>
2019-01-24 21:44:03 -0800

    updated default for onTermSubmit method

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount() {
-    this.onTermSubmit("javascript");
+    this.onTermSubmit("beer ipa");
   }
 
   onTermSubmit = async term => {
